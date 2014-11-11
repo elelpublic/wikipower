c9fc9e4d6bc1e34564b3bd96e82e5f6112941e8e
larsfrommars <larsfrommars@gmail.com>
2014-11-11 16:32:41 +0100

    play with angular, w.i.p.

--- src/main/webapp/static/js/play.js
+++ src/main/webapp/static/js/play.js
@@ -1,23 +1,35 @@
-angular.module('PlayApp', [])
-  .controller('PlayController', ['$scope', function($scope) {
-    $scope.naomi = { name: 'Naomi' };
-    $scope.igor = { name: 'Igor' };
-    $scope.refresh = function() {
-      console.log( 'Refreshing ' + $scope.name );
-    };
-  }])
-  .directive('myCustomer', function() {
-    return {
-      restrict: 'E',
-      transclude: true,
-      scope: {
-        person: '=',
-        //onRefresh: '&onRefresh'
-        onRefresh: function() {
-          console.log( $scope.person );
-        }
-      },
-      templateUrl: 'templates/my-customer.html'
-      //template: 'Name: {{customer.name}} Address: {{customer.address}}'
-    };
-  });
+angular.module('docsIsoFnBindExample', [])
+.controller('Controller', ['$scope', '$timeout', function($scope, $timeout) {
+  $scope.name = 'Tobias';
+  $scope.hideDialog = function () {
+    $scope.dialogIsHidden = true;
+    $timeout(function () {
+      $scope.dialogIsHidden = false;
+      console.log( 'Hello' );
+    }, 2000);
+  };
+}])
+.controller('TableController', ['$scope', function($scope) {
+  $scope.rows = [
+    {"c1": "C1", "c2": "C2", "c3": "C3", "c4": "C4"},
+    {"c1": "C11", "c2": "C22", "c3": "C33", "c4": "C44"},
+    {"c1": "C111", "c2": "C222", "c3": "C333", "c4": "C444"},
+    {"c1": "C1111", "c2": "C2222", "c3": "C3333", "c4": "C4444"}
+  ];
+}])
+.directive('myDialog', function() {
+  return {
+    restrict: 'E',
+    transclude: true,
+    scope: {
+      'close': '&onClose'
+    },
+    templateUrl: 'templates/my-customer.html'
+  };
+})
+.directive('bsmTable', function() {
+  return {
+    restrict: 'E',
+    templateUrl: 'templates/bsm-table.html'
+  };
+});
